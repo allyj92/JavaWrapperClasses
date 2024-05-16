6b038e36b2f8ffd5d518f4c8417a46496fa9a196
eunjaelim <imeunjae361@gmail.com>
2024-05-16 12:45:39 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Boolean a = true;
+        Character b = "@";
+        Integer c = 123;
+        Double d = 3.1;
+        String e = "Bro";
+
+
+    }
+}
